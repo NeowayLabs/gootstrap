be27f586391becefd937afda9093fb8c0448fa33
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-08-21 21:12:01 -0300

    Add main template

---
+++ templates/cmd/{{project}}/{{project}}.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var (
+	// version is set at build time
+	Version = "No version provided at build time"
+)
+
+func main() {
+
+	version := ""
+	flag.BoolVar(&version, "version", false, "Show version")
+	flag.Parse()
+
+	if version {
+		fmt.Printf("build tag: %s\n", Version)
+		return
+	}
+}
