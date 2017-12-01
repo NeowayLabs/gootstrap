5bee9487598e50831f032afa8756bc70b23010de
Kamila Hinckel <kahinckel@gmail.com>
2017-12-01 14:28:44 -0200

    fix grep

--- templates/hack/coverage.sh
+++ templates/hack/coverage.sh
@@ -9,7 +9,7 @@ echo $coveragemode > coverage.txt
 for d in $(go list ./... | grep -v vendor); do
     go test -v -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
-        grep -h -v $coveragemode profile.out >> coverage.txt
+        grep -h -v "^${coveragemode}" profile.out >> coverage.txt
         rm profile.out
     fi
 done
