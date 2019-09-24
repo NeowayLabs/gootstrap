5e4fe1112b6e18b87c947f249c345d10a531973a
Paulo Pizarro <paulo.pizarro@gmail.com>
2019-09-23 21:03:55 -0300

    Add version package

--- template/cmd.go
+++ template/cmd.go
@@ -5,22 +5,15 @@ const Cmd = `package main
 import (
 	"flag"
 	"fmt"
-)
-
-var (
-	// version is set at build time
-	Version = "No version provided at build time"
+	"{{.Module}}/pkg/version"
 )
 
 func main() {
-
-	version := false
-	flag.BoolVar(&version, "version", false, "Show version")
+	showVersion := flag.Bool("version", false, "Show version")
 	flag.Parse()
 
-	if version {
-		fmt.Printf("version: %s\n", Version)
-		return
+	if *showVersion {
+		fmt.Print(version.Version())
 	}
 }
 `
